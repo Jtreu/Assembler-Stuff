a59bb5b9eae1e819f71ab96af4ea4db6a84332e8
Jameson Treu <jamesontreu@gmail.com>
2016-08-19 09:32:54 -0500

    Add files via upload

---
+++ sum.asm
@@ -0,0 +1,50 @@
+### Sum integers entered by the user
+### sum.asm
+### Jameson Treu
+### 700618652
+ 
+	.text
+	li $t0,0
+next:
+###	Prompt to Input an Integer
+	la $a0,prompt
+	li $v0,4
+	syscall
+	
+###	Input n
+	li $v0,5
+	syscall
+	sw $v0,integer
+	
+###	$t0 is running total
+###	$t1 is the user's integer
+	lw $t1,integer
+	beqz $t1,end
+	add $t0,$t0,$t1
+	b next
+	
+###	Output Message and Sum
+end:	la $a0,prompt2
+	li $v0,4
+	syscall
+	
+	move $a0,$t0
+	li $v0,1
+	syscall
+	
+	la $a0,endl
+	li $v0,4
+	syscall
+	
+###	Exit
+	li $v0,10
+	syscall
+	
+	
+	.data
+integer: .word	0
+prompt:
+	.asciiz "Input any integer, input 0 to terminate program: "
+prompt2:
+	.asciiz "The sum of your integers is: "
+endl:	.asciiz "\n"
